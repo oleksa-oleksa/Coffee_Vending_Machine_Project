8a82b4f8711c9ff98e6128196d60eb4d6b2ab850
Brice-D <bricedorseylongdje@ymail.com>
2018-05-16 23:01:14 +0200

    implementation of the Class Actuator

--- actuator.cpp
+++ actuator.cpp
@@ -1,6 +1,49 @@
+#include <iostream>
 #include "actuator.h"
+using namespace std;
 
 Actuator::Actuator()
 {
+    act(state, duration);
+}
 
+Actuator::~Actuator()
+{
+    cout << "end" << endl;
+}
+
+State Actuator::getActuatorState() const
+{
+    return actuatorState;
+}
+
+void Actuator::setActuatorState(State act_State)
+{
+    actuatorState = act_State;
+}
+
+ActuatorType Actuator::getActuatorType() const
+{
+    return actuatorType;
+}
+
+ unsigned int Actuator::getActuatorDuration() const
+{
+    return act_duration;
+}
+
+void Actuator::act(State state, unsigned int duration)
+{
+    setActuatorDuration(duration);
+    setActuatorState(state);
+}
+
+void Actuator::setActuatorType(string name )
+{
+    actuatorType = name;
+}
+
+void Actuator::setActuatorDuration(unsigned int duration)
+{
+    act_duration = duration;
 }
--- actuator.h
+++ actuator.h
@@ -8,17 +8,19 @@ class Actuator
 {
     ActuatorType actuatorType;
     unsigned int act_duration = 0;
+    State actuatorState;
 
     public:
         Actuator();
+        ~Actuator();
         void act(State state, unsigned int duration);
         ActuatorType getActuatorType() const;
         unsigned int getActuatorDuration() const;
-        int getActuatorState() const;
+        State getActuatorState() const;
         void setActuatorState(int state);
         void setActuatorDuration(unsigned int duration);
         void setActuatorType(string name);
-        void setActuatorType(std::string name);
+        void setActuatorType(string name);
 };
 
 #endif // ACTUATOR_H
