9082c409f1e62e83385269ac8b87993ebc37b1ff
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-06-13 10:08:08 +0200

    bankaccount ready

--- bankaccount.cpp
+++ bankaccount.cpp
@@ -1,27 +1,33 @@
 #include "bankaccount.h"
 #include <string>
+#include <QtDebug>
 
 BankAccount::BankAccount()
 {
-
+    taxClass = 1; // default tax class
 }
 
 void BankAccount::setIBAN(std::string iban)
 {
-
+    IBAN = iban;
+    QDebug() << "New IBAN is set: " << IBAN;
 }
 
 std::string BankAccount::getIBAN()
 {
-
+    QDebug() << "IBAN is: " << IBAN;
+    return IBAN;
 }
 
 void BankAccount::setTaxClass(int tc)
 {
+    taxClass = tc;
+    QDebug() << "New tax class is set:" << taxClass;
 
 }
 
 int BankAccount::getTaxClass()
 {
-
+    QDebug() << "Tax class is:" << taxClass;
+    return taxClass;
 }
