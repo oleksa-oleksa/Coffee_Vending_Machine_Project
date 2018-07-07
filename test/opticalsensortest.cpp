b5d65a0e2ff4cb32e4431ce073c5918d23b7dc25
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-07-07 18:52:45 +0200

    opticalsensortest adjusted

--- test/opticalsensortest.cpp
+++ test/opticalsensortest.cpp
@@ -3,19 +3,21 @@
 void OpticalSensorTest::testGetOpticalSensorsMeasurement()
 {
     //Test1
-    OpticalSensor opSensor1;
+    Flowmeter fm1;
+
+    OpticalSensor opSensor1(&fm1);
     QCOMPARE(opSensor1.getOpticalFlowSensorsMeasurements(), NO_CUP);
 
     //Test2
-    Flowmeter *fm1 = new Flowmeter();
-    fm1->mainFlowmeterRoutine(); // setHasPrepared = True
-    OpticalSensor opSensor2(fm1);
+    Flowmeter fm2;
+    fm2.mainFlowmeterRoutine(); // setHasPrepared = True
+    OpticalSensor opSensor2(&fm2);
     opSensor2.setDistanceToObject(4); // isObjectdetected = true
     QCOMPARE(opSensor2.getOpticalFlowSensorsMeasurements(), FULL_CUP);
 
     //Test3
-    Flowmeter fm2; // setHasPrepared = false
-    OpticalSensor opSensor3(&fm2);
+    Flowmeter fm3; // setHasPrepared = false
+    OpticalSensor opSensor3(&fm3);
     opSensor3.setDistanceToObject(4); // isObjectdetected = true
     QCOMPARE(opSensor3.getOpticalFlowSensorsMeasurements(), EMPTY_CUP);
 }
