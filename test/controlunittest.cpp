2fdbe5fdd796d693d0fa5beb6613eb64c17841ca
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-07-11 00:17:42 +0200

    Tested ControlUnit init

--- test/controlunittest.cpp
+++ test/controlunittest.cpp
@@ -1,2 +1,46 @@
 #include "controlunittest.h"
+#include "userchoice.h"
+
+void ControlUnitTest::init()
+{
+    choice = new UserChoice();
+    flow = new Flowmeter();
+    opticalSensor = new OpticalSensor(flow);
+    brightSensor = new BrightnessSensor();
+    display = new LCD_Display();
+    cardScanner = new RFID_Scanner();
+    cu = new ControlUnit();
+
+    cu->connectOptical(opticalSensor);
+    cu->connectBrightnessSensor(brightSensor);
+    cu->connectRFID(cardScanner);
+    cu->connectFlow(flow);
+    cu->connectLCD(display);
+
+    activeUserChoice = this->choice;
+}
+
+void ControlUnitTest::cleanup()
+{
+    delete cardScanner;
+    delete display;
+    delete brightSensor;
+    delete opticalSensor;
+    delete flow;
+    delete choice;
+    delete cu;
+
+    activeUserChoice = NULL;
+}
+
+void ControlUnitTest::testControlUnitInit()
+{
+
+    cu->onStartInit();
+
+    QCOMPARE(choice->getSelectedDrink(), NO_DRINK);
+    QCOMPARE(opticalSensor->getDistanceToObject(), 10);
+    QCOMPARE(opticalSensor->getOpticalFlowSensorsMeasurements(), NO_CUP);
+    QCOMPARE(brightSensor->getBrightness() < 3, display->getBacklight());
+}
 
--- test/controlunittest.h
+++ test/controlunittest.h
@@ -7,7 +7,22 @@
 class ControlUnitTest: public QObject
 {
     Q_OBJECT
+
+    ControlUnit *cu;
+    UserChoice *choice;
+    Flowmeter *flow;
+    OpticalSensor *opticalSensor;
+    BrightnessSensor *brightSensor;
+    LCD_Display *display;
+    RFID_Scanner *cardScanner;
+
     private slots:
+
+    void init();
+    void cleanup();
+
+
+    void testControlUnitInit();
 };
 
 #endif // CONTROLUNITTEST_H
